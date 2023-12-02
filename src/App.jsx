181e1ac8a87968c138e9f550bdd2b43086322644
your_github_username <your_github_email>
2023-12-02 11:55:56 +0530

    learned useRef:lets us refrence the variable that's not needed for rendering , it persists values acroos various life cycles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  let localVal = 0;
+  const refVal = useRef(0);
+  const [myVal, setMyVal] = useState(0);
 
+  console.log(localVal);
+  console.log(refVal.current);
+  console.log(myVal);
   return (
     <>
+      <button onClick={() => localVal++}>upadate local variable</button>
+      <button onClick={() => refVal.current++}>upadate ref</button>
+      <button onClick={() => setMyVal((prevVal) => prevVal + 1)}>
+        upadate state variable
+      </button>
+
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>Local Val :{localVal}</div>
+        <div>ref Val :{refVal.current}</div>
+        <div>state Val :{myVal}</div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
